2b863b622f0f963afacc39181f55a981c2197106
MGI Admin <mgiadmin@jax.org>
2015-07-01 17:21:34 +0000

    tr11750 merged to trunk

--- bin/updateSQL.sh
+++ bin/updateSQL.sh
@@ -1,39 +1,23 @@
 #!/bin/sh
 
-echo 'Dropping SEQ_Sequence triggers' | tee -a ${LOG_PROC}
-${MGD_DBSCHEMADIR}/trigger/SEQ_Sequence_drop.object
+echo "Updating sequence status to 'ACTIVE'" | tee -a ${LOG_PROC} >> ${LOG_PROC} 2>&1
 
-echo "Updating sequence status to 'ACTIVE'" | tee -a ${LOG_PROC}
-cat - <<EOSQL | ${MGI_DBUTILS}/bin/doisql.csh ${MGD_DBSERVER} ${MGD_DBNAME} >> ${LOG_PROC}
-
-use ${MGD_DBNAME}
-go
+cat - <<EOSQL | psql -h${MGD_DBSERVER} -d${MGD_DBNAME} -U mgd_dbo -e >> ${LOG_DIAG} 2>&1
 
 select s._Sequence_key
-into #seqsToUpdate
+into  temp seqsToUpdate
 from  ACC_Accession a, SEQ_Sequence s
 where a._MGIType_key = 19
 and a._LogicalDB_key = ${LOGICALDB_KEY}
 and a.preferred = 1
 and a._Object_key = s._Sequence_key
-and s._SequenceStatus_key = 316343
-go
+and s._SequenceStatus_key = 316343;
 
-create index idx1 on #seqsToUpdate(_Sequence_key)
-go
+create index idx1 on seqsToUpdate(_Sequence_key);
 
-update SEQ_Sequence
+update SEQ_Sequence s
 set _SequenceStatus_key = 316342
-from #seqsToUpdate u, SEQ_Sequence s
-where u._Sequence_key = s._Sequence_key
-go
-
-checkpoint
-go
-
-quit
+from seqsToUpdate u
+where s._Sequence_key = u._Sequence_key;
 
 EOSQL
-
-echo 'Creating SEQ_Sequence triggers' | tee -a ${LOG_PROC}
-${MGD_DBSCHEMADIR}/trigger/SEQ_Sequence_create.object
